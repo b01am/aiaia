5c8d28599e7a911baf2a340013e0854777ad419e
jmk0121 <twin090121@gmail.com>
2025-12-30 16:41:03 +0900

    ㅇㄹㅇㄹㅇ

--- beatus-project/src/App.js
+++ beatus-project/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const API_KEY = "AIzaSyBDjo07dezt0BqvZ872USHyMW7wrNAyXqQ";
+const API_KEY = "AIzaSyBO-Pl-JLA3wHaAOTG93HGSa9UJJ_gAMuY";
 
 function App() {
   // 상태 관리
